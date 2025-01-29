8ae12b0238ab742cd5fe62c29a93288770c07a6e
Matheus <matheushrm614@gmail.com>
2025-01-29 19:58:34 -0300

    add gitignore and move route to use controller

---
+++ app_backend/Http/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using App.Model;
+
+namespace App.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                Summaries[Random.Shared.Next(Summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    }
+}
